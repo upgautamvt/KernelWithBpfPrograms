cb63996e5555440334ccd17076ed71c2f8406cfa
upgautamvt <upgautam@vt.edu>
2024-07-11 14:54:53 -0400

    todo: fix why vanilla kernel has low thoughput and high latency than kernel with xdp programs loaded

--- bpf-progs/xdp/XdpProg_PacketsToNginx.kern.c
+++ bpf-progs/xdp/XdpProg_PacketsToNginx.kern.c
@@ -11,6 +11,7 @@
 
 SEC("xdp")
 int xdp_prog(struct xdp_md *ctx) {
+    //bpf_printk("xdp prog started\n");
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
     struct ethhdr *eth = data;
@@ -28,20 +29,22 @@ int xdp_prog(struct xdp_md *ctx) {
         return XDP_DROP;
 
     // Check if the packet is a TCP packet
-    if (ip->protocol != IPPROTO_TCP)
-        return XDP_PASS;
+    if (ip->protocol == IPPROTO_TCP) {
+        struct tcphdr *tcp = (void *)ip + sizeof(struct iphdr);
+        if ((void *)(tcp + 1) > data_end)
+            return XDP_DROP;
 
-    struct tcphdr *tcp = (void *)ip + sizeof(struct iphdr);
-    if ((void *)(tcp + 1) > data_end)
-        return XDP_DROP;
+        // Check if the destination port is 80 (HTTP) or 22 (SSH)
+        if (tcp->dest == __constant_htons(80) || tcp->dest == __constant_htons(22)) {
+            bpf_printk("TCP traffic destined for port 80 or port 22\n");
+            // Implement any further filtering or actions here
+            // For now, we'll just pass the packet to the Nginx server
+            return XDP_PASS;
+        }
+    }
 
-    // Check if the destination port is 80 (HTTP)
-    if (tcp->dest != __constant_htons(80))
-        return XDP_PASS;
-
-    // Implement any further filtering or actions here
-    // For now, we'll just pass the packet to the Nginx server
-    return XDP_PASS;
+    // Drop all other packets
+    return XDP_DROP;
 }
 
 char _license[] SEC("license") = "GPL";
